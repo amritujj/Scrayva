c8f0d8c968a6477b9acc93be73e431c31adb6129
amritujj <ujjwalamrit15@gmail.com>
2026-03-19 12:46:28 +0530

    Add new Scrayva branded favicon in all standard formats

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicons */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* External Scripts */}
         <link rel="preconnect" href="https://checkout.razorpay.com" />
         <link rel="dns-prefetch" href="https://checkout.razorpay.com" />
         <script src="https://checkout.razorpay.com/v1/checkout.js" defer></script>
